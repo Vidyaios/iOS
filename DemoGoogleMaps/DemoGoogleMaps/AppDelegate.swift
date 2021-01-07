4efe13ad7f07117d4cae1b2d16138199d1ffb291
Sri Vidya Soundarya <vidya2892@gmail.com>
2021-01-07 17:30:44 -0500

    Googlemaps, GooglePlaces and GooglePlacePicker using CocoaPods

--- DemoGoogleMaps/DemoGoogleMaps/AppDelegate.swift
+++ DemoGoogleMaps/DemoGoogleMaps/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Override point for customization after application launch
         GMSServices.provideAPIKey("AIzaSyB5HG_68kSulno6yRinDllSpA9piQB4iG0")//("AIzaSyBEib1TBMsX_OjYjah_bJ9kuyT0lxqkgBk")
         GMSPlacesClient.provideAPIKey("AIzaSyB5HG_68kSulno6yRinDllSpA9piQB4iG0")//("AIzaSyBEib1TBMsX_OjYjah_bJ9kuyT0lxqkgBk")
         return true
