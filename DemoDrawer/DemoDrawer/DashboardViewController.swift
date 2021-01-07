e012898da4c3b1f72249dcdc797b704ae56f932a
Sri Vidya Soundarya <vidya2892@gmail.com>
2021-01-07 17:26:36 -0500

    Third party Drawer Menu

--- DemoDrawer/DemoDrawer/DashboardViewController.swift
+++ DemoDrawer/DemoDrawer/DashboardViewController.swift
@@ -13,7 +13,7 @@ class DashboardViewController: SliderLibViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
 
     override func didReceiveMemoryWarning() {
