163a2bacd82f2e2215ec5895fb3d4b79716da19f
Sri Vidya Soundarya <vidya2892@gmail.com>
2021-01-08 16:18:54 -0500

    Default and Custom Tableview

--- DemoUITableView/DemoUITableView/Type2ViewController.swift
+++ DemoUITableView/DemoUITableView/Type2ViewController.swift
@@ -38,7 +38,7 @@ class Type2ViewController: UIViewController, UITableViewDataSource, UITableViewD
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        // Pass the selected object to the new view controller
     }
     */
     /*
