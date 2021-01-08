1d913f44bea2d0c5fd244ace9c0933d2fbcb9172
Sri Vidya Soundarya <vidya2892@gmail.com>
2021-01-08 16:11:56 -0500

    Navigation with Segue

--- DemoSegue/DemoSegue/PresentModallySegueViewController.swift
+++ DemoSegue/DemoSegue/PresentModallySegueViewController.swift
@@ -13,7 +13,7 @@ class PresentModallySegueViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
     }
 
     override func didReceiveMemoryWarning() {
